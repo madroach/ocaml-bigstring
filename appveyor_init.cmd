fab2277702bfb02f4d62515c2f21ec7ee5f73c78
Christopher Zimmermann <madroach@gmerlin.de>
2019-11-11 11:33:58 +0100

    move to target with OPAMROOT=C:/OPAM

--- appveyor_init.cmd
+++ appveyor_init.cmd
@@ -1,37 +1,24 @@
-REM Download and install OCaml and flexlink (unless it was already done).
-REM Prepare the environment variables,... to use it.  OCaml is installed
-REM at %OCAMLROOT%
-REM
-REM If you are using Cygwin, install it in C:\cygwin first and then
-REM execute this script.  Execute bash with the option "-l".
-
-REM set OCAMLROOT=%PROGRAMFILES%/OCaml
 set OCAMLROOT=C:/PROGRA~1/OCaml
 set OPAMROOT=C:/OPAM
 
 if not defined OCAML_BRANCH (set OCAML_BRANCH=4.09)
 set OCAMLURL="https://ci.appveyor.com/api/projects/madroach/ocaml-appveyor/artifacts/ocaml-%OCAML_BRANCH%.zip"
 
-if not exist "%OCAMLROOT%/bin/ocaml.exe" (
-  echo Downloading OCaml %OCAML_BRANCH% from "%OCAMLURL%"
-  appveyor DownloadFile "%OCAMLURL%" -FileName "%temp%/ocaml.zip"
-  cd "%PROGRAMFILES%"
-  7z x -y "%temp%/ocaml.zip"
-  del %temp%\ocaml.zip
-)
+echo Downloading OCaml %OCAML_BRANCH% from "%OCAMLURL%"
+appveyor DownloadFile "%OCAMLURL%" -FileName "%temp%/ocaml.zip"
+cd "%PROGRAMFILES%"
+7z x -y "%temp%/ocaml.zip"
+del %temp%/ocaml.zip
 
-REM Cygwin is always installed on AppVeyor.  Its path must come
-REM before the one of Git but after those of MSCV and OCaml.
-set Path=C:\cygwin\bin;%Path%
+set Path=C:/cygwin/bin;%Path%
 
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 
-set Path=%OCAMLROOT%\bin;%OCAMLROOT%\bin\flexdll;C:\opam\bin;%Path%
-set CAML_LD_LIBRARY_PATH=%OCAMLROOT%/lib/stublibs
-REM set OCAMLTOP_INCLUDE_PATH="%OPAMROOT%\ocaml-system\lib\toplevel"
-set OCAMLRUNPARAM="bs=8M"
+REM Cygwin is always installed on AppVeyor.  Its path must come
+REM before the one of Git but after those of MSCV and OCaml.
+set Path=%OCAMLROOT%/bin;%OCAMLROOT%/bin/flexdll;%Path%
 
-set CYGWINBASH=C:\cygwin\bin\bash.exe
+set CYGWINBASH=C:/cygwin/bin/bash.exe
 
 if exist %CYGWINBASH% (
   REM Make sure that "link" is the MSVC one and not the Cynwin one.
@@ -46,7 +33,22 @@ if exist %CYGWINBASH% (
   %CYGWINBASH% -lc "tr -d '\\r' </tmp/env >> ~/.bash_profile"
 )
 
-cd "%APPVEYOR_BUILD_FOLDER%"
-opam init -vv --yes --compiler=ocaml-system https://github.com/madroach/opam-repository.git
+opam init --yes --compiler=ocaml-system https://github.com/madroach/opam-repository.git
+REM stdlib-shims 0.1 is broken on Windows - update just to 0.2 ?
+opam pin --no-action stdlib-shims https://github.com/ocaml/stdlib-shims.git
+
+appveyor SetVariable -Name OPAMROOT -Value %OPAMROOT%
+set OPAM_SWITCH_PREFIX=%OPAMROOT%/ocaml-system
+appveyor SetVariable -Name OPAM_SWITCH_PREFIX -Value %OPAM_SWITCH_PREFIX%
+set OCAMLLIB=%OCAMLROOT%/lib/ocaml
+appveyor SetVariable -Name CAML_LD_LIBRARY_PATH -Value %CAML_LD_LIBRARY_PATH%
+set CAML_LD_LIBRARY_PATH=%OCAMLROOT%/lib/stublibs:%OPAM_SWITCH_PREFIX%/lib/stublibs
+appveyor SetVariable -Name CAML_LD_LIBRARY_PATH -Value %CAML_LD_LIBRARY_PATH%
+set OCAMLTOP_INCLUDE_PATH="%OPAM_SWITCH_PREFIX%/lib/toplevel"
+appveyor SetVariable -Name OCAMLTOP_INCLUDE_PATH -Value %OCAMLTOP_INCLUDE_PATH%
+set OCAMLPATH="%OPAM_SWITCH_PREFIX%/lib
+appveyor SetVariable -Name OCAMLPATH -Value %OCAMLPATH%
+set OCAMLRUNPARAM="bs=8M"
+appveyor SetVariable -Name OCAMLRUNPARAM -Value %OCAMLRUNPARAM%
 
 set <NUL /p=Ready to use OCaml & ocamlc -version
